2462e190d0ee17b05e20359f3d0e0720c3038f2e
Jason <Jason235@qq.com>
2015-03-11 11:49:11 +0800

    添加loginapp

---
+++ QuoteSystem/QuoteSystem/js/LoginApp.js
@@ -0,0 +1,14 @@
+﻿/***
+AngularJS App Login Script
+***/
+
+var LoginApp = angular.module("LoginApp", []);
+
+LoginApp.controller('LoginController',
+    ['$scope',
+     '$rootScope',
+     function ($scope, $rootScope) {
+         $scope.OnSubmit = function () {
+             alert('Submit');
+         };
+     }])
